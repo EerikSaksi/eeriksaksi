53590c5e6714896211c2c50025983213453cfb88
Eerik <2392230s@student.gla.ac.uk>
2020-09-16 11:55:34 +0100

    tried to manually do it

--- src/components/cards/timeline.tsx
+++ src/components/cards/timeline.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import CustomCard from 'components/cards/custom_card';
-import { useTheme, Container, Slider, Typography } from '@material-ui/core';
+import { Fade, useTheme, Container, Slider, Typography } from '@material-ui/core';
 
 const dates = [
   { description: "Here's my brief timeline:" },
@@ -14,7 +14,7 @@ const dates = [
 
 //estimates the readingTime required to read text
 const readingTime = (phrase: string) => {
-  return phrase.length / 8 * 1000
+  return (phrase.length / 8) * 1000;
 };
 
 //returns the total difference from the first day to the last day
@@ -35,24 +35,66 @@ const TimeLine: React.FC = () => {
   //stores the index of the current date object
   const [dateIndex, setDateIndex] = useState(0);
 
-  const [sliderValue, setSliderValue] = useState<number[]>([]);
-  const [description, setDescription] = useState("");
+  const [sliderValue, setSliderValue] = useState<number[]>([0, 0]);
+
+  //changes oldValues to newValues in 100 equal steps
+  const animateValueChange = async (oldValues: number[], newValues: number[]) => {
+    const leftStepAmount = (newValues[0] - oldValues[0]) / 100;
+    const rightStepAmount = (newValues[1] - oldValues[1]) / 100;
+    for (let index = 0; index < 100; index++) {
+      oldValues[0] += leftStepAmount;
+      oldValues[1] += rightStepAmount;
+      setSliderValue(oldValues);
+      await new Promise((resolve) => setTimeout(resolve, 10));
+    }
+  };
+  const [description, setDescription] = useState('');
+
+  console.log(sliderValue);
+  const [textVisible, setTextVisible] = useState(false);
   useEffect(() => {
     const periodicallyIncrementIndex = async () => {
-      for (const date of dates){
-        console.log(date)
-        setDescription(date.description)
+      for (const date of dates) {
+        setTextVisible(true);
+        setDescription(date.description);
         if (date.startDate) {
-          setSliderValue([getDeltaFromFirst(date.startDate), getDeltaFromFirst(date.endDate)]);
+          //get the difference with the new and old date and divide by 100
+          const startStepAmount = (getDeltaFromFirst(date.startDate) - sliderValue[0]) / 10;
+          const endStepAmount = (getDeltaFromFirst(date.endDate) - sliderValue[1]) / 10;
+          //over 10 iterations add the step amount until oldValue is newValue
+          for (let index = 0; index < 10; index++) {
+            await new Promise((resolve) => setTimeout(resolve, 100));
+            setSliderValue((oldValue) => {
+              oldValue[0] += startStepAmount;
+              oldValue[1] += endStepAmount;
+              return oldValue;
+            });
+          }
+          setSliderValue((oldValues) => {
+            animateValueChange(oldValues, [getDeltaFromFirst(date.startDate), getDeltaFromFirst(date.endDate)]);
+            return oldValues;
+          });
         }
-        await new Promise(resolve => setTimeout(resolve, readingTime(date.description)));
+        await new Promise((resolve) => setTimeout(resolve, readingTime(date.description)));
+        setTextVisible(false);
+        await new Promise((resolve) => setTimeout(resolve, 500));
       }
     };
     periodicallyIncrementIndex();
   }, []);
+  console.log(sliderValue[1])
   return (
     <CustomCard>
-      <Typography style = {{ textAlign: 'center' }} variant='h2'>{description}</Typography>
+      <Fade in={textVisible}>
+        <>
+          <Typography style={{ textAlign: 'center' }} variant='h2'>
+            {description}
+          </Typography>
+          <Typography style={{ textAlign: 'center' }} variant='h2'>
+            {`${sliderValue[0]} ${sliderValue[1]}`}
+          </Typography>
+        </>
+      </Fade>
       <Container style={{ width: '90%', marginTop: theme.spacing(3), transition: 'all 500ms' }}>
         <Slider value={sliderValue} getAriaValueText={(value) => 'test'} max={maxDate} valueLabelDisplay='on' aria-labelledby='range-slider' />
       </Container>
