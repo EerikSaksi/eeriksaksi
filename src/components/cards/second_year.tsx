13a3d78cb5b0b26568853bb08561494b6a7b10e0
Eerik <2392230s@student.gla.ac.uk>
2020-09-19 17:48:40 +0100

    adding backgroundimage component to custom card to prevent blur

--- src/components/cards/second_year.tsx
+++ src/components/cards/second_year.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import CustomCard from 'components/cards/custom_card';
 import { Grid, useTheme } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
-import ProgressiveImage from "react-progressive-image-loading";
+import ProgressiveImage from 'react-progressive-image-loading';
 const SecondYear: React.FC = () => {
   const theme = useTheme();
   return (
-    <ProgressiveImage src = {require('media/glasgow.jpg')} placeholder={require('media/glasgow-tiny.jpg')}>
-      {(src) => (
-        <CustomCard containerStyle={{ backgroundImage: `url(${src})`, backgroundSize: 'cover', padding: theme.spacing(4) }}>
-          <Grid>
-            <Typography variant='h2'>Second year at University of Glasgow</Typography>
-          </Grid>
-        </CustomCard>
-      )}
-    </ProgressiveImage>
+    <ProgressiveImage
+      src={require('media/glasgow.jpg')}
+      preview={require('media/glasgow-tiny.jpg')}
+      render={(src, style) => {
+        return (
+          <CustomCard  containerStyle={{ backgroundImage: `url(${src})`, backgroundSize: 'cover', padding: theme.spacing(4), ...style }}>
+            <Grid>
+              <Typography variant='h2'>Second year at University of Glasgow</Typography>
+            </Grid>
+          </CustomCard>
+        );
+      }}
+    />
   );
 };
+
 export default SecondYear;
