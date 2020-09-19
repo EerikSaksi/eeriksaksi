7c1b5425b92c35e37c9f55c3ec6236df576fb7dc
Eerik <2392230s@student.gla.ac.uk>
2020-09-19 19:24:23 +0100

    trying image

--- src/components/cards/second_year.tsx
+++ src/components/cards/second_year.tsx
@@ -6,19 +6,22 @@ import ProgressiveImage from 'react-progressive-image-loading';
 const SecondYear: React.FC = () => {
   const theme = useTheme();
   return (
-    <ProgressiveImage
-      src={require('media/glasgow.jpg')}
-      preview={require('media/glasgow-tiny.jpg')}
-      render={(src, style) => {
-        return (
-          <CustomCard  containerStyle={{ backgroundImage: `url(${src})`, backgroundSize: 'cover', padding: theme.spacing(4), ...style }}>
-            <Grid>
-              <Typography variant='h2'>Second year at University of Glasgow</Typography>
-            </Grid>
-          </CustomCard>
-        );
-      }}
-    />
+    <>
+      <ProgressiveImage
+        src={require('media/glasgow.jpg')}
+        preview={require('media/glasgow-tiny.jpg')}
+        render={(src, style) => {
+          return (
+            <CustomCard containerStyle={{ padding: theme.spacing(4) }}>
+              <div style={{ position: 'absolute', top: 0, right: 0, left: 0, bottom: 0, maxHeight: '100%',  backgroundImage: `url(${src})`, backgroundSize: 'cover', zIndex: -1, ...style, }}></div>
+                  <Grid>
+                <Typography variant='h2'>Second year at University of Glasgow</Typography>
+              </Grid>
+            </CustomCard>
+          );
+        }}
+      />
+    </>
   );
 };
 
