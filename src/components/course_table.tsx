159dc157853daabcc0ca8789beede65072eadbd3
Eerik <2392230s@student.gla.ac.uk>
2020-09-20 11:48:24 +0100

    reusing second year components

---
+++ src/components/course_table.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import {Table, TableHead, TableCell, TableRow, TableBody, Paper, TableContainer} from '@material-ui/core'
+const CourseTable: React.FC = () => {
+
+}
+export default CourseTable
---
+++ src/components/q_and_a_accordion.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'; 
+import { Grid, Typography, Accordion, AccordionSummary, AccordionDetails } from '@material-ui/core'; 
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+type QuestionAnswers = Array<{ question: string; answer: React.ReactNode }>;
+
+const QAndAAccordion: React.FC<{ questionAnswers: QuestionAnswers }> = ({questionAnswers }) => {
+  const [currentlyOpenIndex, setCurrentlyOpenIndex] = useState(-1);
+  return (
+    <React.Fragment>
+      {questionAnswers.map(({ question, answer }, index) => (
+        <Grid item xs={12}>
+          <Accordion onChange={() => (index === currentlyOpenIndex ? setCurrentlyOpenIndex(-1) : setCurrentlyOpenIndex(index))} expanded={index === currentlyOpenIndex}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls='panel1a-content' id='panel1a-header'>
+              <Typography variant='h5'>{question}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>{answer}</AccordionDetails>
+          </Accordion>
+        </Grid>
+      ))}
+    </React.Fragment>
+  );
+};
+export default QAndAAccordion;
