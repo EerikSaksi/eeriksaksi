cdfb39aff0fd7d025c859b10c7408c3547a738d6
Eerik <2392230s@student.gla.ac.uk>
2020-09-21 11:01:56 +0100

    ios compatibility

--- src/components/cards/custom_card.tsx
+++ src/components/cards/custom_card.tsx
@@ -3,7 +3,7 @@ import { Grid, useTheme, Paper } from '@material-ui/core';
 const CustomCard = React.forwardRef<unknown, { children: React.ReactNode; style?: React.CSSProperties, containerStyle?: React.CSSProperties }>(({ children, style, containerStyle  }, ref) => {
   const theme = useTheme();
   return (
-    <Grid alignItems='center' container style={{  height: `calc(100vh - ${theme.spacing(6) + 64}px)`, marginBottom: theme.spacing(6) + 64,  minWidth: '100%',  scrollSnapAlign: 'center',  scrollSnapStop: 'always', ...containerStyle }}>
+    <Grid alignItems='center' container style={{  height: `calc(100vh - ${theme.spacing(6) + 64}px)`, marginBottom: theme.spacing(6) + 64,  minWidth: '100%',  scrollSnapAlign: 'center',  scrollSnapStop: 'always', overflow:'visible', ...containerStyle }}>
       <Grid item xs={12}>
         <Paper ref={ref} style={{  alignSelf: 'center',  ...style}}>
           {children}
