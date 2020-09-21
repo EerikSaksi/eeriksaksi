dc1decd20e4bfc9504ccbbd7bdd04de51c194b2f
Eerik <2392230s@student.gla.ac.uk>
2020-09-21 10:58:09 +0100

    Removed spinner

--- src/components/cards/loading.tsx
+++ src/components/cards/loading.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { CircularProgress, Card } from '@material-ui/core';
+import { Card } from '@material-ui/core';
 const Loading: React.FC<{height: number | string}> = ({height}) => {
   //pass height so that there will be a variable size card that is the same size as the loading content so it looks like an empty card before the content is there 
   return (
     <Card style = {{ height }}>
       <div style = {{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', }}>
-      <CircularProgress  style = {{ width: 100, height: 100 }}/>
     </div>
     </Card>
   );
