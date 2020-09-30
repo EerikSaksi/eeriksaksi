6b81af64b10af772dc8a52a5501fa74fb30d7a6b
Eerik <2392230s@student.gla.ac.uk>
2020-09-30 16:39:35 +0100

    Dissertation

--- src/components/cards/fourth_year.tsx
+++ src/components/cards/fourth_year.tsx
@@ -17,29 +17,37 @@ const questionAnswers = [
   {
     question: 'Year description / how I chose my courses.',
     answer: (
-        <Typography style = {{ padding: 16}} variant='h4'>Through both semesters, I also have a dissertation project which is worth 33% of our grade. For the first semester (and up until this point in my degree), we haven't have much freedom of choice, but for the second semester we had a lot of freedom, and I decided to study everything I could to increase my understanding and skill in full stack development.</Typography>
+      <Typography style={{ padding: 16 }} variant='h4'>
+        Through both semesters, I also have a dissertation project which is worth 33% of our grade. For the first semester (and up until this point in my degree), we haven't have much freedom of choice, but for the second semester we had a lot of freedom, and I decided to study everything I could to increase my understanding and skill in full stack development.
+      </Typography>
     ),
   },
   {
     question: 'What courses do I have?',
+    answer: <CourseTable headers={['First Semester', 'Second Semester']} rowCols={rows} />,
+  },
+  {
+    question: 'What is my dissertation project?',
     answer: (
-      <CourseTable headers={['First Semester', 'Second Semester']} rowCols={rows}/>
+      <Typography style={{ padding: 16 }} variant='h4'>
+        I am working on a collaborative activity tracker app. The intent of the app is to encourage accountability for training partners through a common goal on the app that can only be achieved through both people consistently training. I might make it similar to clicker game, where DPS is increased through adherance but combined DPS is required to progress. I'm thinking of using React Native to make the app cross platform, as well as PostGraphile (I have been eyeing this library for a while, it auto generates a GraphQL schema from a PostgreSQL database eliminating the N + 1 problem and eliminating the need for a lot of boilerplate code.)
+      </Typography>
     ),
   },
 ];
-const FourthYear: React.FC<{alertCurrentlyVisible:() => void}> = ({alertCurrentlyVisible}) => {
+const FourthYear: React.FC<{ alertCurrentlyVisible: () => void }> = ({ alertCurrentlyVisible }) => {
   return (
-    <CustomCardWithBackground progressiveImageProps={{ src: require('media/cloisters.jpg'), preview: require('media/cloisters-tiny.jpg') } as ProgressiveImageProps} backgroundImageStyle={{ backgroundPosition: '50% 50%' }} photoCredit='_skynet on flickr'  alertCurrentlyVisible = {alertCurrentlyVisible}>
+    <CustomCardWithBackground progressiveImageProps={{ src: require('media/cloisters.jpg'), preview: require('media/cloisters-tiny.jpg') } as ProgressiveImageProps} backgroundImageStyle={{ backgroundPosition: '50% 50%' }} photoCredit='_skynet on flickr' alertCurrentlyVisible={alertCurrentlyVisible}>
       <React.Fragment>
         <Hidden xsDown>
-        <Grid container justify='center'>
-          <Grid item xs={12}>
-            <Typography style={{ textAlign: 'center' }} variant='h2'>
-              4th (current) Year at University of Glasgow
-            </Typography>
+          <Grid container justify='center'>
+            <Grid item xs={12}>
+              <Typography style={{ textAlign: 'center' }} variant='h2'>
+                4th (current) Year at University of Glasgow
+              </Typography>
+            </Grid>
           </Grid>
-        </Grid>
-      </Hidden>
+        </Hidden>
         <QAndAAccordion questionAnswers={questionAnswers} />
       </React.Fragment>
     </CustomCardWithBackground>
