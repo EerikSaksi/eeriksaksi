c8adaa60f615ebfd423ab3bf5947fe56effdeea2
Eerik <2392230s@student.gla.ac.uk>
2020-09-30 20:17:59 +0100

    small fixes

--- src/components/cards/third_year.tsx
+++ src/components/cards/third_year.tsx
@@ -22,7 +22,7 @@ const questionAnswers = [
       </CourseTable>
     ),
   },
-  { question: 'What were my favourite courses?', answer: <Typography variant='h4'>I found Database Systems to be helpful to me for creating better and more extensible database schemas. Cyber Security Fundamentals has made me more vary of my security choices in applications.</Typography> },
+  { question: 'What were the most useful courses?', answer: <Typography variant='h4'>I found Database Systems to be helpful to me for creating better and more extensible database schemas. Cyber Security Fundamentals has made me more vary of my security choices in applications.</Typography> },
   { question: 'What was my favourite course?', answer: <Typography variant='h4'> Although I haven't touched low level languages since this class, Systems Programming has made me interested in implementing efficient API's with something like Rust's Actix Web.</Typography> },
 ];
 const SecondYear: React.FC<{alertCurrentlyVisible:() => void}> = ({alertCurrentlyVisible}) => {
