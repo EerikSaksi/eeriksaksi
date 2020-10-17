ac4ba46ef1c27e9a504e826d7d5f3e49a8877039
Eija Saksi <eijasaksi@Eija-MBP.home>
2020-10-17 17:11:55 +0300

    Fixed?

--- src/components/cards/custom_card.tsx
+++ src/components/cards/custom_card.tsx
@@ -9,11 +9,12 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: theme.shadows[24]
   },
 }));
+
 const CustomCard = forwardRef<unknown, { children: React.ReactNode; style?: React.CSSProperties; containerStyle?: React.CSSProperties }>(({ children, style, containerStyle }, ref) => {
   const classes = useStyles();
   const theme = useTheme();
   return (
-    <Grid alignItems='center' container style={{ height: `calc(100vh - ${theme.spacing(6) + 64}px)`, marginBottom: theme.spacing(6) + 64, scrollSnapAlign: 'center', scrollSnapStop: 'always', ...containerStyle }}>
+    <Grid alignItems='center' container style={{ height: `calc(100vh - ${theme.spacing(6) + 64}px)`, maxWidth: 'calc(100% - 350px)',  marginBottom: theme.spacing(6) + 64, scrollSnapAlign: 'center', scrollSnapStop: 'always', ...containerStyle }}>
       <Grid item xs={12}>
         <Paper className={classes.paper} ref={ref} style={{ alignSelf: 'center', ...style }}>
           {children}
